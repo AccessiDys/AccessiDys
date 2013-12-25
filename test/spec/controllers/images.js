db78e726d9b854f1df2bbbf80b45d48ea5dd94ef
maslouhy <ahmedanas.maslouhy@neoxia.com>
2013-12-25 17:06:56 +0000

    Oceriser Unit test
    git-svn-id: https://www.neoxia-maroc.net/svn/cned/trunk@75 a419c687-58c5-48e2-818b-bbad97856650

--- test/spec/controllers/images.js
+++ test/spec/controllers/images.js
@@ -11,29 +11,44 @@ describe('Controller:ImagesCtrl', function() {
     });
   }));
 
-  /* Test wheter selected() method is well defined or not*/
-  it('should set sendCrop function', function() {
-    expect($scope.selected).toBeDefined();
-  });
+  it('has to return text from image', inject(function($httpBackend) {
 
-  /*Test wheter /images POST responds or not*/
-  it('should call /images on $scope.sendCrop()', inject(function($httpBackend) {
-    $scope.sendCrop();
+ var expected = { }; 
 
-    $httpBackend.expectPOST('/images').respond();
-  }));  
+ $httpBackend.expectPOST('/oceriser').respond(expected);
 
-  /*Test if loader is false*/
-  it('test add cropped image', function() {
-     expect($scope.loader).toBe(false);
-  });
+ $scope.oceriser('./image.png');
 
-  /*Test wheter /images POST responds or not*/
-   it('should call /oceriser on $scope.oceriser()', inject(function($httpBackend) {
-    $scope.oceriser();
+ $httpBackend.flush();
 
-    $httpBackend.expectPOST('/oceriser').respond();
-  }));  
+  expect(scope.textes.text).toBe(expected);
+    expect(scope.textes.source).toBe('./image.png');
+
+}));
+
+  // /* Test wheter selected() method is well defined or not*/
+  // it('should set sendCrop function', function() {
+  //   expect($scope.selected).toBeDefined();
+  // });
+
+  // /*Test wheter /images POST responds or not*/
+  // it('should call /images on $scope.sendCrop()', inject(function($httpBackend) {
+  //   $scope.sendCrop();
+
+  //   $httpBackend.expectPOST('/images').respond();
+  // }));  
+
+  // /*Test if loader is false*/
+  // it('test add cropped image', function() {
+  //    expect($scope.loader).toBe(false);
+  // });
+
+  // /*Test wheter /images POST responds or not*/
+  //  it('should call /oceriser on $scope.oceriser()', inject(function($httpBackend) {
+  //   $scope.oceriser();
+
+  //   $httpBackend.expectPOST('/oceriser').respond();
+  // }));  
 
 
 });
