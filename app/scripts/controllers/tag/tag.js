d62df9783d26da6bcf4c58f90a0ce2189a28eed6
amoufaddel <abdelhaq.moufaddel@neoxia.com>
2014-01-09 09:37:21 +0000

    tests unitaires du Tag
    git-svn-id: https://www.neoxia-maroc.net/svn/cned/trunk@109 a419c687-58c5-48e2-818b-bbad97856650

--- app/scripts/controllers/tag/tag.js
+++ app/scripts/controllers/tag/tag.js
@@ -1,49 +1,54 @@
 'use strict';
 
 angular.module('cnedApp').controller('TagCtrl', function($scope, $http) {
-
+	
 	$scope.afficherTags = function() {
 		$http.get('/readTags')
 			.success(function(data) {
-			if (data != 'err') {
-				$scope.listTags = data;
-			}
-		});
+				if (data == 'err') {
+					console.log("Désolé un problème est survenu lors de l'affichge des tags");
+				} else {
+					$scope.listTags = data;
+				}
+			});
 	};
 
 	$scope.ajouterTag = function() {
 		$http.post('/addTag', $scope.tag)
 			.success(function(data) {
-			if (data == 'err') {
-				console.log("Désolé un problème est survenu lors de l'enregistrement");
-			} else {
-				$scope.tag = {};
-				$scope.afficherTags();
-			}
-		});
+				if (data == 'err') {
+					console.log("Désolé un problème est survenu lors de l'enregistrement");
+				} else {
+					$scope.tagFlag = data; /* destiné aux tests unitaires */
+					$scope.tag = {};
+					$scope.afficherTags();
+				}
+			});
 	};
 
 	$scope.supprimerTag = function() {
 		$http.post('/deleteTag', $scope.fiche)
 			.success(function(data) {
-			if (data == 'err') {
-				console.log("Désolé un problème est survenu lors de la suppression");
-			} else {
-				$scope.afficherTags();
-			}
-		});
+				if (data == 'err') {
+					console.log("Désolé un problème est survenu lors de la suppression");
+				} else {
+					$scope.tagFlag = data; /* destiné aux tests unitaires */
+					$scope.afficherTags();
+				}
+			});
 	};
 
 	$scope.modifierTag = function() {
 		$http.post('/updateTag', $scope.fiche)
 			.success(function(data) {
-			if (data == 'err') {
-				console.log("Désolé un problème est survenu lors de la suppression");
-			} else {
-				$scope.afficherTags();
-				$scope.fiche = {};
-			}
-		});
+				if (data == 'err') {
+					console.log("Désolé un problème est survenu lors de la modification");
+				} else {
+					$scope.tagFlag = data; /* destiné aux tests unitaires */
+					$scope.afficherTags();
+					$scope.fiche = {};
+				}
+			});
 	};
 
 	$scope.preModifierTag = function(tag) {
---
+++ test/spec/controllers/tag.js
@@ -0,0 +1,107 @@
+'use strict';
+
+describe('Controller:TagCtrl', function() {
+	var $scope, controller;
+	var tags = [
+		          	{
+		          		_id: "52c588a861485ed41c000001",
+		          		libelle: "Exercice"
+		          	},
+		          	{
+		          		_id: "52c588a861485ed41c000002",
+			          	libelle: "Cours"
+		          	}
+          		];
+	var tag = {
+			_id: "52c588a861485ed41c000003",
+			libelle: "TP"
+      	  };
+	
+	beforeEach(module('cnedApp'));
+
+	beforeEach(inject(function($controller, $rootScope, $httpBackend) {
+	    $scope = $rootScope.$new();
+	    controller = $controller('TagCtrl', {
+	      $scope: $scope
+	    });
+	    
+	    $httpBackend.whenGET('/readTags').respond(tags);
+	    
+	    $scope.tag = tag;
+		$httpBackend.whenPOST('/addTag').respond(tag);
+		
+	    $scope.fiche = tag;
+		$httpBackend.whenPOST('/deleteTag').respond(tag);
+		
+		$httpBackend.whenPOST('/updateTag').respond(tag);
+	}));
+	
+	/* TagCtrl:afficherTag */
+	
+	it('TagCtrl:afficherTags should set afficherTags function', inject(function($httpBackend) {
+		expect($scope.afficherTags).toBeDefined();
+	}));	
+	
+	it('TagCtrl:afficherTags should call /readTags on $scope.afficherTags()', inject(function($httpBackend) {
+		$scope.afficherTags();
+		$httpBackend.flush();
+	}));
+  
+	it('TagCtrl:afficherTags should listTags be tags', inject(function($httpBackend) {
+		$scope.afficherTags();
+		$httpBackend.flush();
+		expect($scope.listTags.length).toBe(2);
+	}));
+
+	/* TagCtrl:ajouterTag */
+	
+	it('TagCtrl:ajouterTag should set ajouterTag function', inject(function($httpBackend) {
+		expect($scope.ajouterTag).toBeDefined();
+	}));	
+	
+	it('TagCtrl:ajouterTag should call /addTag on $scope.ajouterTag()', inject(function($httpBackend) {
+		$scope.ajouterTag();
+		$httpBackend.flush();
+	}));
+
+	it('TagCtrl:ajouterTag should tag be $scope.tagAdded', inject(function($httpBackend) {
+		$scope.ajouterTag();
+		$httpBackend.flush();
+		expect(tag).toEqual($scope.tagFlag);
+	}));
+	
+	/* TagCtrl:supprimerTag */
+	
+	it('TagCtrl:supprimerTag should set supprimerTag function', inject(function($httpBackend) {
+		expect($scope.supprimerTag).toBeDefined();
+	}));	
+	
+	it('TagCtrl:supprimerTag should call /deleteTag on $scope.supprimerTag()', inject(function($httpBackend) {
+		$scope.supprimerTag();
+		$httpBackend.flush();
+	}));
+
+	it('TagCtrl:supprimerTag should tag be $scope.tagFlag', inject(function($httpBackend) {
+		$scope.supprimerTag();
+		$httpBackend.flush();
+		expect(tag).toEqual($scope.tagFlag);
+	}));
+	
+	/* TagCtrl:modifierTag */
+	
+	it('TagCtrl:modifierTag should set modifierTag function', inject(function($httpBackend) {
+		expect($scope.modifierTag).toBeDefined();
+	}));	
+	
+	it('TagCtrl:modifierTag should call /updateTag on $scope.modifierTag()', inject(function($httpBackend) {
+		$scope.modifierTag();
+		$httpBackend.flush();
+	}));
+
+	it('TagCtrl:modifierTag should tag be $scope.tagFlag', inject(function($httpBackend) {
+		$scope.modifierTag();
+		$httpBackend.flush();
+		expect(tag).toEqual($scope.tagFlag);
+	}));
+
+});
