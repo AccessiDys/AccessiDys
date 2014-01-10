be0ee5bad173cb5dddedcd625c12126d9c9eb24c
amoufaddel <abdelhaq.moufaddel@neoxia.com>
2014-01-10 20:29:45 +0000

    implementation du DAO findTagsByProfil()
    git-svn-id: https://www.neoxia-maroc.net/svn/cned/trunk@129 a419c687-58c5-48e2-818b-bbad97856650

--- routes/adaptation.js
+++ routes/adaptation.js
@@ -47,6 +47,7 @@ module.exports = function(app) {
     //route for ProfileTag manipulations
     var profilsTags = require('../api/dao/profilTag');
     app.post('/ajouterProfilTag', profilsTags.createProfilTag);
+    app.post('/chercherTagsParProfil', profilsTags.findTagsByProfil);
 
 
 };
