f34ea11993363116a1a6159f1857c75b371625ac
maslouhy <ahmedanas.maslouhy@neoxia.com>
2014-01-27 10:09:41 +0000

    Modify ProfilTag DAO
    git-svn-id: https://www.neoxia-maroc.net/svn/cned/trunk@246 a419c687-58c5-48e2-818b-bbad97856650

--- api/dao/profilTag.js
+++ api/dao/profilTag.js
@@ -4,7 +4,7 @@
  * Module dependencies.
  */
 var mongoose = require('mongoose'),
-    ProfilTag = mongoose.model('ProfilTag');
+  ProfilTag = mongoose.model('ProfilTag');
 
 
 
@@ -12,83 +12,63 @@ var mongoose = require('mongoose'),
  * Create ProfileTag
  */
 exports.createProfilTag = function(req, res) {
-    var profilTag = new ProfilTag(req.body);
-    console.log('create');
-    profilTag.save(function(err) {
-        if (err) {
-            return res.send('users/signup', {
-                errors: err.errors,
-                profilTag: profilTag
-            });
-        } else {
-            res.jsonp(profilTag);
-        }
-    });
+  var profilTag = new ProfilTag(req.body);
+  console.log('create');
+  profilTag.save(function(err) {
+    if (err) {
+      return res.send('users/signup', {
+        errors: err.errors,
+        profilTag: profilTag
+      });
+    } else {
+      res.jsonp(profilTag);
+    }
+  });
 };
 
-
 /**
- * Update ProfilTag
+ * Find profilTag by Profil
  */
 
-exports.update = function(req, res){
-    var profilTag = new ProfilTag(req.body);
-    ProfilTag.findById(profilTag._id, function  (err, item) {
-    if (err){
-        res.send({'result':'error'});
-    }
-    else {
-           item.tag = profilTag.tag;
-           item.texte = profilTag.texte;
-           item.tagName = profilTag.tagName;
-           item.police = profilTag.police;
-           item.taille = profilTag.taille;
-           item.interligne = profilTag.interligne;
-           item.styleValue = profilTag.styleValue;
-           
-           item.save(item, function () {
-               res.send({'result':'error'});
-          });
+exports.findTagsByProfil = function(req, res) {
+  ProfilTag.find({
+    profil: req.body.idProfil
+  }, function(err, tags) {
+    if (err) {
+      res.send({
+        'result': 'error'
+      });
+    } else {
+      res.jsonp(200, tags);
     }
   });
 };
 
 
 /**
- * Find profilTag by Profil
-  */
-
-exports.findTagsByProfil = function(req, res){
-ProfilTag.find({profil:req.body.idProfil}, function  (err, tags) {
-     if (err){
-            res.send({'result':'error'});
-     }
-     else {
-        res.jsonp(tags);
-     }
-   });
-};
-
-
-/**
  * Delete profilTag by ProfilID & TagID
-  */
+ */
 
-exports.supprimer = function(req, res){
+exports.supprimer = function(req, res) {
 
-     var profilTag = new ProfilTag(req.body);
+  var profilTag = new ProfilTag(req.body);
 
-     ProfilTag.findOne({ profil: profilTag.profil , tag: profilTag.tag }, function  (err, item) {
-     if (err){
-            res.send({'result':'error'});
-     }
-     else {
-                console.log('suppression ==> ');
-                console.log(item);
+  ProfilTag.findOne({
+    profil: profilTag.profil,
+    tag: profilTag.tag
+  }, function(err, item) {
+    if (err) {
+      res.send({
+        'result': 'error'
+      });
+    } else {
 
-           ProfilTag.remove(item, function () {
-               res.send({'result':'error'});
-           });
-     }
-   });
+
+      ProfilTag.remove(item, function() {
+        res.send({
+          'result': 'error'
+        });
+      });
+    }
+  });
 };
