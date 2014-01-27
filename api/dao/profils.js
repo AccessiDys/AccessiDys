e5a31839a65ac975a43ef74689e0ad98ed0f79aa
maslouhy <ahmedanas.maslouhy@neoxia.com>
2014-01-27 10:09:11 +0000

    Modify profils DAO
    git-svn-id: https://www.neoxia-maroc.net/svn/cned/trunk@245 a419c687-58c5-48e2-818b-bbad97856650

--- api/dao/profils.js
+++ api/dao/profils.js
@@ -4,67 +4,75 @@
  * Module dependencies.
  */
 var mongoose = require('mongoose'),
-    Profil = mongoose.model('Profil');
+  Profil = mongoose.model('Profil');
 var fs = require('fs');
 
 /**
  * Add a profile
  */
 exports.createProfile = function(req, res) {
-    var profile = new Profil(req.body);
+  var profile = new Profil(req.body);
 
-    var bitmap = fs.readFileSync(profile.photo);
-    profile.photo = new Buffer(bitmap).toString('base64');
+  var bitmap = fs.readFileSync(profile.photo);
+  profile.photo = new Buffer(bitmap).toString('base64');
 
-    console.log('create');
-    profile.save(function(err) {
-        if (err) {
-            return res.send('users/signup', {
-                errors: err.errors,
-                profile: profile
-            });
-        } else {
-            // console.log("profil"+ profile._id);
-            res.jsonp(profile);
-        }
-    });
+  console.log('create');
+  profile.save(function(err) {
+    if (err) {
+      return res.send('users/signup', {
+        errors: err.errors,
+        profile: profile
+      });
+    } else {
+      // console.log("profil"+ profile._id);
+      res.jsonp(profile);
+    }
+  });
 };
 
 /**
  * List of Profiles
  */
 exports.all = function(req, res) {
-    Profil.find().exec(function(err, profils) {
-        if (err) {
-            res.render('error', {
-                status: 500
-            });
-        } else {
-            // console.log(profils._id);
-            res.jsonp(profils);
-        }
-    });
+  Profil.find().exec(function(err, profils) {
+    if (err) {
+      res.render('error', {
+        status: 500
+      });
+    } else {
+      // console.log(profils._id);
+      res.jsonp(profils);
+    }
+  });
 };
 
 /**
  * Update Profiles
  */
 
-exports.update = function(req, res){
-    var profil = new Profil(req.body);
-    Profil.findById(profil._id, function  (err, item) {
-    if (err){
-        res.send({'result':'error'});
-    }
-    else {
-           item.photo = profil.photo;
-           item.nom = profil.nom;
-           item.type = profil.type;
-           item.descriptif = profil.descriptif;
-           item.niveauScolaire = profil.niveauScolaire;
-           item.save(item, function () {
-               res.send({'result':'error'});
+exports.update = function(req, res) {
+  var profil = new Profil(req.body);
+
+  Profil.findById(profil._id, function(err, item) {
+    if (err) {
+      res.send({
+        'result': 'error'
+      });
+    } else {
+      item.photo = profil.photo;
+      item.nom = profil.nom;
+      item.type = profil.type;
+      item.descriptif = profil.descriptif;
+      item.niveauScolaire = profil.niveauScolaire;
+      item.save(function(err) {
+        if (err) {
+          res.send({
+            'result': 'error'
           });
+        } else {
+          res.jsonp(200, item);
+        }
+      });
     }
   });
 };
@@ -72,18 +80,21 @@ exports.update = function(req, res){
 
 /**
  * Delete Profiles
-  */
+ */
 
-exports.supprimer = function(req, res){
-     var profil = new Profil(req.body);
-     Profil.findById(profil._id, function  (err, item) {
-     if (err){
-            res.send({'result':'error'});
-     }
-     else {
-           Profil.remove(item, function () {
-               res.send({'result':'error'});
-           });
-     }
-   });
+exports.supprimer = function(req, res) {
+  var profil = new Profil(req.body);
+  Profil.findById(profil._id, function(err, item) {
+    if (err) {
+      res.send({
+        'result': 'error'
+      });
+    } else {
+      Profil.remove(item, function() {
+        res.send({
+          'result': 'error'
+        });
+      });
+    }
+  });
 };
