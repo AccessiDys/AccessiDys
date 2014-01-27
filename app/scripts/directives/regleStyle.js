eb458c713bccd2225fe57fcfa2956c2d10b8b89b
maslouhy <ahmedanas.maslouhy@neoxia.com>
2014-01-27 17:58:47 +0000

    Add parameters directive implementation
    git-svn-id: https://www.neoxia-maroc.net/svn/cned/trunk@259 a419c687-58c5-48e2-818b-bbad97856650

--- app/scripts/directives/regleStyle.js
+++ app/scripts/directives/regleStyle.js
@@ -1,20 +1,34 @@
-cnedApp.directive('regleStyle', [ '$rootScope', function($rootScope) {
-  return {
-    restrict: 'EA',
-    replace: true,
-    require: '?ngModel',
-    scope: {
-      interligneList: '@'
-    },
-    link: function(scope, elem, attrs) {      
+cnedApp.directive('regleStyle', ['$rootScope',
+  function($rootScope) {
+    return {
+      restrict: 'EA',
+      replace: true,
+      require: '?ngModel',
+      scope: {
+        interligneList: '@'
+      },
+      link: function() {
 
-      $rootScope.$on('reglesStyleChange', function(nv, params) {
-       
-        console.log('the params ==> ');
-        console.log(params);
-        
-        $('.shown-text').css('line-height', params.value + 'px');
-      });
-    }
-  };
-}]);
+        $rootScope.$on('reglesStyleChange', function(nv, params) {
+
+          switch (params.operation) {
+            case 'interligne':
+              $('.shown-text').css('line-height', params.value + 'px');
+              break;
+            case 'style':
+              $('.shown-text').css('font-weight', params.value);
+              break;
+            case 'taille':
+              $('.shown-text').css('font-size', params.value + 'px');
+              break;
+            case 'police':
+              $('.shown-text').css('font-family', params.value);
+              break;
+
+          }
+        });
+
+      }
+    };
+  }
+]);
