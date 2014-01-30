d4f7dcd42a53af1d7df1591f67a327bfc3da0b57
amoufaddel <abdelhaq.moufaddel@neoxia.com>
2014-01-30 09:30:41 +0000

    implementation de Drag && Drop dans TreeView
    git-svn-id: https://www.neoxia-maroc.net/svn/cned/trunk@270 a419c687-58c5-48e2-818b-bbad97856650

--- app/scripts/directives/treeView.js
+++ app/scripts/directives/treeView.js
@@ -111,3 +111,52 @@ cnedApp.directive('ezTree', function ($compile, $timeout) {
     }
   };
 });
+
+cnedApp.directive('uiNestedSortable', ['$parse', function ($parse) {
+
+    'use strict';
+
+    var eventTypes = 'Create Start Sort Change BeforeStop Stop Update Receive Remove Over Out Activate Deactivate'.split(' ');
+
+    return {
+        restrict: 'A',
+        link: function (scope, element, attrs) {
+
+            var options = attrs.uiNestedSortable ? $parse(attrs.uiNestedSortable)() : {};
+
+            angular.forEach(eventTypes, function (eventType) {
+
+                var attr = attrs['uiNestedSortable' + eventType],
+                    callback;
+
+                if (attr) {
+                    callback = $parse(attr);
+                    options[eventType.charAt(0).toLowerCase() + eventType.substr(1)] = function (event, ui) {
+                        scope.$apply(function () {
+
+                            callback(scope, {
+                                $event: event,
+                                $ui: ui
+                            });
+                        });
+                    };
+                }
+
+            });
+            
+            //note the item="{{child}}" attribute on line 17
+            options.isAllowed = function(item, parent) {
+                if (!parent) return false;
+                var attrs = parent.context.attributes;
+                parent = attrs.getNamedItem('item');
+                attrs = item.context.attributes;
+                item = attrs.getNamedItem('item');
+                //if ( ... ) return false;
+               return true;
+                };
+
+            element.nestedSortable(options);
+
+        }
+    };
+}]);
