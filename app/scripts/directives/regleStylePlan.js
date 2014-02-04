ead5cfe0529fac9b122adae31b76613a12bf90f9
mandoubi <mohamed.elmandoubi@neoxia.com>
2014-02-04 09:50:36 +0000

    Correction regressions application régles coloration
    git-svn-id: https://www.neoxia-maroc.net/svn/cned/trunk@310 a419c687-58c5-48e2-818b-bbad97856650

--- app/scripts/directives/regleStylePlan.js
+++ app/scripts/directives/regleStylePlan.js
@@ -1,11 +1,9 @@
 /*global cnedApp,Hyphenator, $:false */
 'use strict';
 
-cnedApp.directive('regleStyle', ['$rootScope', 'removeHtmlTags', function($rootScope, removeHtmlTags) {
+cnedApp.directive('regleStylePlan', ['$rootScope', 'removeHtmlTags', function($rootScope, removeHtmlTags) {
   return {
     restrict: 'EA',
-    replace: true,
-    priority: 100000,
     link: function() {
 
       var currentParam = '';
@@ -19,24 +17,22 @@ cnedApp.directive('regleStyle', ['$rootScope', 'removeHtmlTags', function($rootS
       Hyphenator.config(hyphenatorSettings);
 
       /* Regles de styles par defaut */
-      $rootScope.$on('ElementHtmlAdded', function(ev, el) {
-        ev.stopPropagation();
-        // console.log('ElementHtmlAdded');
-        // console.log($(el).find('p').attr('data-weight'));
-        el.css({
-          fontWeight: $(el).find('p').attr('data-weight'),
-          fontSize: $(el).find('p').attr('data-size') + 'px',
-          lineHeight: $(el).find('p').attr('data-lineheight') + 'px',
-          fontFamily: $(el).find('p').attr('data-font')
-        });
+      // $rootScope.$on('ElementHtmlAdded', function(ev, el) {
+      //   // ev.stopPropagation();
+      //   // console.log('ElementHtmlAdded');
+      //   // console.log($(el).find('p').attr('data-weight'));
+      //   el.css({
+      //     fontWeight: $(el).find('p').attr('data-weight'),
+      //     fontSize: $(el).find('p').attr('data-size') + 'px',
+      //     lineHeight: $(el).find('p').attr('data-lineheight') + 'px',
+      //     fontFamily: $(el).find('p').attr('data-font')
+      //   });
 
-        // Regles coloration
-        regleColoration($(el).find('p').attr('data-coloration'), $(el).find('p'));
-      });
+      //   // Regles coloration
+      //   regleColoration($(el).find('p').attr('data-coloration'), $(el).find('p'));
+      // });
 
       var lineAction = function(elementAction) {
-
-        console.log('lineAction');
 
         var p = $(elementAction);
         var words = p.text().split(' ');
@@ -44,20 +40,16 @@ cnedApp.directive('regleStyle', ['$rootScope', 'removeHtmlTags', function($rootS
         $.each(words, function(i, w) {
           if ($.trim(w)) text = text + '<span>' + w + ' </span>';
         });
-        p.html(text);
+        $(elementAction).html(text);
 
         $(window).resize(function() {
           var line = 0;
           var prevTop = -15;
-          var top = 0;
           $('span', p).each(function() {
             var word = $(this);
-            // var top = word.offset().top;
-            top += word.length;
-            // console.log('top ==> ');
-            // console.log(top);
-            if (top > 17) {
-              top = 0;
+            var top = word.offset().top;
+            if (top !== prevTop) {
+              prevTop = top;
               if (line === 3) {
                 line = 1;
               } else {
@@ -165,52 +157,40 @@ cnedApp.directive('regleStyle', ['$rootScope', 'removeHtmlTags', function($rootS
       };
 
       /* Relges de style Profils */
-      $rootScope.$on('reglesStyleChange', function(nv, params) {
-        nv.stopPropagation();
-        switch (params.operation) {
-
-          case 'interligne':
-            $('.' + params.element).css('line-height', params.value + 'px');
-            break;
-
-          case 'style':
-            $('.' + params.element).css('font-weight', params.value);
-            break;
-
-          case 'taille':
-            $('.' + params.element).css('font-size', params.value + 'px');
-            break;
-
-          case 'police':
-            $('.' + params.element).css('font-family', params.value);
-            break;
-
-          case 'coloration':
-            regleColoration(params.value, $('.' + params.element));
-            break;
-        }
-
-      });
-
-      function regleColoration(param, elementAction) {
+      $rootScope.$on('stylePlan', function(e, param) {
+        console.log('stylePlan declenched ... ');
         console.log(param);
         switch (param) {
           case 'Couleur par défaut':
-            $('.line1').css('background-color', '');
-            $('.line2').css('background-color', '');
-            $('.line3').css('background-color', '');
-            $(elementAction).css('color', 'black');
-            $(elementAction).find('span').css('color', 'black');
-            $(elementAction).text($(elementAction).text());
-            return false;
+            $('.plan .line1').css('background-color', '');
+            $('.plan .line2').css('background-color', '');
+            $('.plan .line3').css('background-color', '');
+            $('.plan').css('color', 'black');
+            $('.plan').find('span').css('color', 'black');
+            // $(elementAction).text($(elementAction).text());
             break;
 
           case 'Colorer les lignes':
-            lineAction(elementAction);
-            $('.line1').css('color', '#D90629');
-            $('.line2').css('color', '#066ED9');
-            $('.line3').css('color', '#4BD906');
-            return false;
+            console.log('number li => ' + $('.plan').length);
+            var ligne = 0;
+            $('.plan li').each(function(index, li) {
+              console.log('ligne ==> ' + ligne);
+              if (ligne === 0) {
+                $(li).find('a').css('color', '#D90629');
+                ligne += 1;
+              } else if (ligne === 1) {
+                $(li).find('a').css('color', '#066ED9');
+                ligne += 1;
+              } else if (ligne === 2) {
+                $(li).find('a').css('color', '#4BD906');
+                ligne = 0;
+              }
+
+            });
+
+            // $('.line1').css('color', '#D90629');
+            // $('.line2').css('color', '#066ED9');
+            // $('.line3').css('color', '#4BD906');
             break;
 
           case 'Colorer les mots':
@@ -221,7 +201,6 @@ cnedApp.directive('regleStyle', ['$rootScope', 'removeHtmlTags', function($rootS
             $(elementAction).find('.line1').css('color', '#D90629');
             $(elementAction).find('.line2').css('color', '#066ED9');
             $(elementAction).find('.line3').css('color', '#4BD906');
-            return false;
             break;
 
           case 'Surligner les mots':
@@ -238,7 +217,6 @@ cnedApp.directive('regleStyle', ['$rootScope', 'removeHtmlTags', function($rootS
               'background-color': '#04ffff',
               'color': '#000'
             });
-            return false;
             break;
 
           case 'Surligner les lignes':
@@ -248,16 +226,16 @@ cnedApp.directive('regleStyle', ['$rootScope', 'removeHtmlTags', function($rootS
             $('.line1').css('background-color', '#fffd01');
             $('.line2').css('background-color', '#04ff04');
             $('.line3').css('background-color', '#04ffff');
-            return false;
             break;
 
           case 'Colorer les syllabes':
             decoupe('color-syllabes', elementAction);
-            return false;
             break;
 
         }
-      }
+
+      });
+
 
     }
   };
