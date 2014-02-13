87a300c5b00f642a89271e76616d3764bdcf243c
mandoubi <mohamed.elmandoubi@neoxia.com>
2014-02-13 17:49:33 +0000

    Optimisation édition texte espace découpage
    git-svn-id: https://www.neoxia-maroc.net/svn/cned/trunk@376 a419c687-58c5-48e2-818b-bbad97856650

--- app/bower_components/ckeditor/config.js
+++ app/bower_components/ckeditor/config.js
@@ -7,6 +7,7 @@ CKEDITOR.editorConfig = function(config) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
+	config.height = '200px';
 	config.disableNativeSpellChecker = false;
 	config.scayt_autoStartup = true;
 	CKEDITOR._scaytParams = {
