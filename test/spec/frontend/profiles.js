a59c757d1466c6e0ad7d15320cc1f144125bc8fd
maslouhy <ahmedanas.maslouhy@neoxia.com>
2014-02-19 15:11:48 +0000

    Profiles unit tests
    git-svn-id: https://www.neoxia-maroc.net/svn/cned/trunk@393 a419c687-58c5-48e2-818b-bbad97856650

--- test/spec/frontend/profiles.js
+++ test/spec/frontend/profiles.js
@@ -1,28 +1,27 @@
 /* File: profiles.js
-*
-* Copyright (c) 2014
-* Centre National d’Enseignement à Distance (Cned), Boulevard Nicephore Niepce, 86360 CHASSENEUIL-DU-POITOU, France
-* (direction-innovation@cned.fr)
-*
-* GNU Affero General Public License (AGPL) version 3.0 or later version
-*
-* This file is part of a program which is free software: you can
-* redistribute it and/or modify it under the terms of the
-* GNU Affero General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU Affero General Public License for more details.
-*
-* You should have received a copy of the GNU Affero General Public
-* License along with this program.
-* If not, see <http://www.gnu.org/licenses/>.
-*
-*/
-
+ *
+ * Copyright (c) 2014
+ * Centre National d’Enseignement à Distance (Cned), Boulevard Nicephore Niepce, 86360 CHASSENEUIL-DU-POITOU, France
+ * (direction-innovation@cned.fr)
+ *
+ * GNU Affero General Public License (AGPL) version 3.0 or later version
+ *
+ * This file is part of a program which is free software: you can
+ * redistribute it and/or modify it under the terms of the
+ * GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License along with this program.
+ * If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 
 
 
@@ -373,8 +372,8 @@ describe('Controller:ProfilesCtrl', function() {
     expect($scope.tagStyles.indexOf($scope.parameter)).toBe(-1);
     expect($scope.tagStyles.length).toBe(2);
     expect($scope.listTags[1].disabled).toBeFalsy();
-    $httpBackend.flush();
-    expect($scope.editionSupprimerTagFlag).toEqual(profilTag);
+    expect($scope.trashFlag).toBeTruthy();
+    expect($scope.currentTagProfil).toBe(null);
 
   }));
   it('ProfilesCtrl:editHyphen()', inject(function() {
@@ -414,17 +413,10 @@ describe('Controller:ProfilesCtrl', function() {
 
 
   }));
-  // it('ProfilesCtrl:validerStyleTag()', inject(function($httpBackend) {
-  //   expect($scope.validerStyleTag).toBeDefined();
-  //   $scope.validerStyleTag();
-  //   expect($scope.currentTag).toEqual($scope.tagList);
-
-  // }));
   it('ProfilesCtrl:editerStyleTag()', inject(function($httpBackend) {
     expect($scope.editerStyleTag).toBeDefined();
     $scope.editerStyleTag();
     $httpBackend.flush();
-    expect($scope.modProfilFlag).toBe(profilTag);
     expect($scope.editTag).toEqual(null);
     expect($scope.policeList).toEqual(null);
     expect($scope.tailleList).toEqual(null);
