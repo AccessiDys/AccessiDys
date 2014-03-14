4aa0169a185ea174a378fcfee7ccf4eaf404bf58
amoufaddel <abdelhaq.moufaddel@neoxia.com>
2014-03-14 16:29:08 +0000

    Afficher un message pop-up si l'utilisateur est non connecté.

--- app/scripts/controllers/workspace/images.js
+++ app/scripts/controllers/workspace/images.js
@@ -468,6 +468,7 @@ angular.module('cnedApp').controller('ImagesCtrl', function($scope, $http, $root
         $scope.loader = true;
         var url = configuration.URL_REQUEST + '/index.html';
         var apercuName = 'K-L-' + generateUniqueId() + '.html';
+        var errorMsg = 'Veuillez-vous connecter pour pouvoir enregistrer sur Dropbox';
 
         $http.get(configuration.URL_REQUEST + '/profile')
             .success(function(data) {
@@ -499,36 +500,13 @@ angular.module('cnedApp').controller('ImagesCtrl', function($scope, $http, $root
                     });
                 } else {
                     $scope.loader = false;
-                    alert('Veuillez-vous connecter pour pouvoir enregistrer sur Dropbox');
+                    alert(errorMsg);
                 }
             }).error(function() {
+                $scope.loader = false;
+                alert(errorMsg);
                 console.log('KO');
             });
-
-        // $http.get(url).then(function(response) {
-        //     response.data = response.data.replace('profilId = null', 'profilId = \'' + $scope.profilSelected + '\'');
-        //     response.data = response.data.replace('blocks = []', 'blocks = ' + angular.toJson($scope.blocks));
-        //     $http({
-        //         method: 'PUT',
-        //         url: 'https://api-content.dropbox.com/1/files_put/dropbox/' + apercuName + '?access_token=' + token,
-        //         data: response.data
-        //     }).success(function() {
-        //         $http.post('https://api.dropbox.com/1/shares/dropbox/' + apercuName + '?short_url=false&access_token=' + token)
-        //             .success(function(data) {
-        //                 console.log(data.url);
-        //                 var urlDropbox = data.url.replace('https://www.dropbox.com', 'http://dl.dropboxusercontent.com');
-        //                 urlDropbox += '#/apercu';
-        //                 console.log(urlDropbox);
-        //                 $window.open(urlDropbox);
-        //                 $scope.loader = false;
-        //             }).error(function() {
-        //                 console.log('share link dropbox failed');
-        //             });
-        //     }).error(function() {
-        //         console.log('file upload failed');
-        //     });
-        // });
-
     };
 
     // Selection des tags
