87a3417bc3a4d1b3a3788e06673e30bf4a023755
mandoubi <mohamed.elmandoubi@neoxia.com>
2014-03-18 18:09:41 +0000

    Enlever la méthode affectSrcValue => Refactoring

--- app/scripts/controllers/workspace/images.js
+++ app/scripts/controllers/workspace/images.js
@@ -306,36 +306,6 @@ angular.module('cnedApp').controller('ImagesCtrl', function($scope, $http, $root
         $scope.showEditor = false;
     };
 
-
-    
-    $scope.affectSrcValue = function(srcs) {
-        $rootScope.$emit('distroyJcrop');
-
-        for (var i = 0; i < srcs.length; i++) {
-            if (srcs[i].extension === '.pdf') {
-                alert('Le fichier est chargé avec succès, Conversion des pages en cours ... ');
-                // Convert Pdf to images
-                convertImage(0, srcs[i].numberPages, srcs[i].path);
-            } else {
-                $scope.blocks.children.push({
-                    level: 0,
-                    id: generateUniqueId(),
-                    text: '',
-                    synthese: '',
-                    source: srcs[i].path,
-                    children: []
-                });
-            }
-
-        }
-        initialiseZones();
-        $scope.files = [];
-        $scope.loader = false;
-
-        // refresh scope binding : for callbacks of methods not with angularJS
-        $scope.$apply();
-    };
-
     function convertImage(page, totalPages, source) {
         $http.post(configuration.URL_REQUEST + '/pdfimage', {
             pdfData: {
