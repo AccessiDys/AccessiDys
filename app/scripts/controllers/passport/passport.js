b23ea53ea6c118b45f16a65fe6a964a8f6c8d174
anasYoubi <anas.youbi@neoxia.com>
2014-03-19 16:42:10 +0000

    Amelioration des messages d'erreur inscription

--- app/scripts/controllers/passport/passport.js
+++ app/scripts/controllers/passport/passport.js
@@ -8,10 +8,26 @@ angular.module('cnedApp').controller('passportCtrl', function($scope, $rootScope
 	$rootScope.area = 'AUTHENTIFICATION / INSCRIPTION';
 
 	$scope.guest = $rootScope.loged;
-	$scope.usernameSign = null;
-	$scope.emailSign = null;
-	$scope.passwordSign = null;
-	$scope.passwordConfirmationSign = null;
+	$scope.obj = {
+		nomSign: '',
+		prenomSign: '',
+		emailSign: '',
+		passwordSign: '',
+		passwordConfirmationSign: ''
+	};
+	$scope.erreur = {
+		erreurSigninNom: false,
+		erreurSigninNomMessage: '',
+		erreurSigninPrenom: false,
+		erreurSigninPrenomMessage: '',
+		erreurSigninEmail: false,
+		erreurSigninEmailMessage: '',
+		erreurSigninPasse: false,
+		erreurSigninPasseMessage: '',
+		erreurSigninConfirmationPasse: false,
+		erreurSigninConfirmationPasseMessage: '',
+		erreurSigninEmailNonDisponibleMessage: false
+	};
 	$scope.emailLogin = null;
 	$scope.passwordLogin = null;
 	$scope.showlogin = true; //true
@@ -20,10 +36,6 @@ angular.module('cnedApp').controller('passportCtrl', function($scope, $rootScope
 	$scope.inscriptionStep1 = true; //true
 	$scope.inscriptionStep2 = false; //false
 	$scope.showStep2part1 = true; //true
-	$scope.erreurSigninNom = false;
-	$scope.erreurSigninPrenom = false;
-	$scope.erreurSigninEmail = false;
-	$scope.erreurSigninPasse = false;
 	$scope.erreurSigninConfirmationPasse = false;
 	$scope.erreurSigninEmailNonDisponible = false;
 	$scope.step1 = 'btn btn-primary btn-circle';
@@ -31,10 +43,6 @@ angular.module('cnedApp').controller('passportCtrl', function($scope, $rootScope
 	$scope.step3 = 'btn btn-default btn-circle';
 	$scope.step4 = 'btn btn-default btn-circle';
 
-
-
-	$scope.nomSign = null;
-	$scope.prenomSign = null;
 	$scope.logout = $rootScope.loged;
 	$scope.missingDropbox = $rootScope.dropboxWarning;
 	$scope.showpart2 = false;
@@ -53,14 +61,14 @@ angular.module('cnedApp').controller('passportCtrl', function($scope, $rootScope
 					$scope.missingDropbox = false;
 					$rootScope.loged = true;
 					$rootScope.admin = result.admin;
-					$rootScope.apply;// jshint ignore:line
+					$rootScope.apply; // jshint ignore:line
 					if ($location.path() !== '/inscriptionContinue') {
 						$location.path('/inscriptionContinue');
 					}
 				} else {
 					$rootScope.loged = true;
 					$rootScope.admin = result.admin;
-					$rootScope.apply;// jshint ignore:line
+					$rootScope.apply; // jshint ignore:line
 				}
 			} else {
 				if ($location.path() !== '/' && $location.path() !== '/workspace') {
@@ -72,54 +80,78 @@ angular.module('cnedApp').controller('passportCtrl', function($scope, $rootScope
 
 	$scope.signin = function() {
 		$scope.erreurSigninEmailNonDisponible = false;
-		if ($scope.verifyEmail($scope.emailSign) && $scope.verifyPassword($scope.passwordSign) && $scope.verifyString($scope.nomSign) && $scope.verifyString($scope.prenomSign) && $scope.passwordConfirmationSign === $scope.passwordSign) {
+		if ($scope.verifyEmail($scope.obj.emailSign) && $scope.verifyPassword($scope.obj.passwordSign) && $scope.verifyString($scope.obj.nomSign) && $scope.verifyString($scope.obj.prenomSign) && $scope.obj.passwordConfirmationSign === $scope.obj.passwordSign) {
 			var data = {
-				email: $scope.emailSign,
-				password: $scope.passwordSign,
-				nom: $scope.nomSign,
-				prenom: $scope.prenomSign
+				email: $scope.obj.emailSign,
+				password: $scope.obj.passwordSign,
+				nom: $scope.obj.nomSign,
+				prenom: $scope.obj.prenomSign
 			};
 			$http.post('/signup', data)
 				.success(function(data) {
-				$scope.singinFlag = data;
-				$scope.inscriptionStep1 = false;
-				$scope.inscriptionStep2 = true;
-				$scope.step2 = 'btn btn-primary btn-circle';
-				$scope.step1 = 'btn btn-default btn-circle';
-				$('#myModal').modal('show');
-			})
+					$scope.singinFlag = data;
+					$scope.inscriptionStep1 = false;
+					$scope.inscriptionStep2 = true;
+					$scope.step2 = 'btn btn-primary btn-circle';
+					$scope.step1 = 'btn btn-default btn-circle';
+					$('#myModal').modal('show');
+				})
 				.error(function() {
-
-				$scope.erreurSigninEmailNonDisponible = true;
-			});
+					$scope.erreur.erreurSigninEmail = false;
+					$scope.erreur.erreurSigninEmailNonDisponible = true;
+				});
 		} else {
-			if (!$scope.verifyString($scope.nomSign)) {
-				$scope.erreurSigninNom = true;
+
+			if (!$scope.verifyString($scope.obj.nomSign)) {
+				if ($scope.obj.nomSign === null) {
+					$scope.erreur.erreurSigninNomMessage = 'Nom : Cette donnée est obligatoire. Merci de compléter le champ.';
+				} else {
+					$scope.erreur.erreurSigninNomMessage = "Nom : Veuillez n'utiliser que des lettres (de a à z), des chiffres et des points.";
+				}
+				$scope.erreur.erreurSigninNom = true;
 			} else {
-				$scope.erreurSigninNom = false;
+				$scope.erreur.erreurSigninNom = false;
 			}
 
-			if (!$scope.verifyString($scope.prenomSign)) {
-				$scope.erreurSigninPrenom = true;
+			if (!$scope.verifyString($scope.obj.prenomSign)) {
+				if ($scope.obj.prenomSign === null) {
+					$scope.erreur.erreurSigninPrenomMessage = 'Prénom : Cette donnée est obligatoire. Merci de compléter le champ.';
+				} else {
+					$scope.erreur.erreurSigninPrenomMessage = "Prénom : Veuillez n'utiliser que des lettres (de a à z), des chiffres et des points.";
+
+				}
+				$scope.erreur.erreurSigninPrenom = true;
 			} else {
-				$scope.erreurSigninPrenom = false;
+				$scope.erreur.erreurSigninPrenom = false;
 			}
 
-			if (!$scope.verifyEmail($scope.emailSign)) {
-				$scope.erreurSigninEmail = true;
+			if (!$scope.verifyEmail($scope.obj.emailSign)) {
+				if ($scope.obj.emailSign === null) {
+					$scope.erreur.erreurSigninEmailMessage = 'Email : Cette donnée est obligatoire. Merci de compléter le champ.';
+				} else {
+					$scope.erreur.erreurSigninEmailMessage = 'Email : Veuillez entrer une adresse mail valable.';
+				}
+				$scope.erreur.erreurSigninEmail = true;
 			} else {
-				$scope.erreurSigninEmail = false;
+				$scope.erreur.erreurSigninEmail = false;
 			}
 
-			if (!$scope.verifyPassword($scope.passwordSign)) {
-				$scope.erreurSigninPasse = true;
+			if (!$scope.verifyPassword($scope.obj.passwordSign)) {
+				$scope.erreur.erreurSigninPasseMessage = 'Les mots de passe doivent comporter au moins six caractères.';
+				$scope.erreur.erreurSigninPasse = true;
 			} else {
-				$scope.erreurSigninPasse = false;
+				$scope.erreur.erreurSigninPasse = false;
 			}
-			if ($scope.passwordSign !== $scope.passwordConfirmationSign) {
-				$scope.erreurSigninConfirmationPasse = true;
+
+			if ($scope.obj.passwordSign !== $scope.obj.passwordConfirmationSign) {
+				if ($scope.obj.passwordConfirmationSign === '') {
+					$scope.erreur.erreurSigninConfirmationPasseMessage = 'Veuillez confirmer votre mot de passe ici.';
+				} else {
+					$scope.erreur.erreurSigninConfirmationPasseMessage = 'Ces mots de passe ne correspondent pas.';
+				}
+				$scope.erreur.erreurSigninConfirmationPasse = true;
 			} else {
-				$scope.erreurSigninConfirmationPasse = false;
+				$scope.erreur.erreurSigninConfirmationPasse = false;
 			}
 		}
 	};
@@ -131,30 +163,30 @@ angular.module('cnedApp').controller('passportCtrl', function($scope, $rootScope
 			};
 			$http.post('/login', data)
 				.success(function(dataRecue) {
-				$scope.loginFlag = dataRecue;
-				$rootScope.loged = true;
-				$rootScope.apply; // jshint ignore:line
-				if ($scope.loginFlag.data) {
-					if ($scope.loginFlag.data.local) {
-						if ($scope.loginFlag.data.local === 'admin') {
+					$scope.loginFlag = dataRecue;
+					$rootScope.loged = true;
+					$rootScope.apply; // jshint ignore:line
+					if ($scope.loginFlag.data) {
+						if ($scope.loginFlag.data.local) {
+							if ($scope.loginFlag.data.local === 'admin') {
+								$location.path('/adminPanel');
+							} else {
+								$location.path('/workspace');
+							}
+						}
+					} else {
+
+						if ($scope.loginFlag.local.role === 'admin') {
 							$location.path('/adminPanel');
 						} else {
 							$location.path('/workspace');
 						}
 					}
-				} else {
-
-					if ($scope.loginFlag.local.role === 'admin') {
-						$location.path('/adminPanel');
-					} else {
-						$location.path('/workspace');
-					}
-				}
 
 
-			}).error(function() {
-				$scope.erreurLogin = true;
-			});
+				}).error(function() {
+					$scope.erreurLogin = true;
+				});
 		} else {
 			$scope.erreurLogin = true;
 		}
@@ -171,7 +203,7 @@ angular.module('cnedApp').controller('passportCtrl', function($scope, $rootScope
 		}
 	};
 	$scope.verifyString = function(chaine) {
-		var ck_nomPrenom = /^[A-Za-z0-9éèâîôç]{3,20}$/;
+		var ck_nomPrenom = /^[A-Za-z0-9éèâîôç ]{3,20}$/;
 		if (chaine === null) {
 			return false;
 		}
