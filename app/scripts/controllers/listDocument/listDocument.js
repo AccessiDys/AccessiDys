69e3cdbf34252f111ac628abf44a2a8c8e1ad6d1
anasYoubi <anas.youbi@neoxia.com>
2014-03-27 09:25:55 +0000

    ajout de l'interface listDocument et le manifest listDocument.appcache qui va contenir les lien des document structurer + a chaque authentification les document test.html et le manifest seront cree dans le dropbox de l'utilisateur

--- app/scripts/controllers/listDocument/listDocument.js
+++ app/scripts/controllers/listDocument/listDocument.js
@@ -24,8 +24,10 @@
  */
 
 'use strict';
+/* global $ */
+/* global listDocument */
 
-angular.module('cnedApp').controller('listDocumentCtrl', function($scope, $rootScope, serviceCheck, $http, $location, uploadDropBox, configuration) {
+angular.module('cnedApp').controller('listDocumentCtrl', function($scope, $rootScope, serviceCheck, $http, $location, dropbox) {
 
 	$('#titreCompte').hide();
 	$('#titreProfile').hide();
@@ -44,6 +46,24 @@ angular.module('cnedApp').controller('listDocumentCtrl', function($scope, $rootS
 	}];
 
 	$scope.initListDocument = function() {
+
+
+		var appCache = window.applicationCache;
+
+		appCache.addEventListener('updateready', function() {
+			console.log('update ready');
+			window.location.reload();
+		}, false);
+
+
+
+		// var refresh = window.location.hash;
+		// if (refresh.indexOf('?refresh=true') > -1) {
+		// 	var url = window.location.href;
+		// 	url = url.replace('?refresh=true', '');
+		// 	window.location.href = url;
+		// }
+
 		var tmp = serviceCheck.getData();
 		tmp.then(function(result) { // this is only run after $http completes
 			if (result.loged) {
@@ -60,72 +80,99 @@ angular.module('cnedApp').controller('listDocumentCtrl', function($scope, $rootS
 					$rootScope.loged = true;
 					$rootScope.admin = result.admin;
 					$rootScope.apply; // jshint ignore:line
-					$('#listDocumentPage').show();
-
-					if (document.getElementById('JSON') === null) {
-						console.log('loaded from dropbox');
-						$http.get('https://api.dropbox.com/1/search/sandbox/?access_token=tLV5CIPVEoAAAAAAAAAAAcZ5zmTIIKjC1VVmeR3zdMokH9dTnk_jIrmAm6oLaVsN&query=.html').success(function(data) {
-							$scope.listDocument = data;
-
-							var myscript = document.createElement('script');
-							myscript.id = 'JSON';
-							myscript.text = JSON.stringify($scope.listDocument);
-							document.body.appendChild(myscript);
-							console.log(JSON.parse(document.getElementById('JSON').text));
-
-							$http.get('https://localhost:3000/index.html')
-								.success(function(data) {
-									var tmp = uploadDropBox.upload('test.html', data, 'tLV5CIPVEoAAAAAAAAAAAcZ5zmTIIKjC1VVmeR3zdMokH9dTnk_jIrmAm6oLaVsN');
-									tmp.then(function(result) { // this is only run after $http completes
-										console.log(result);
-									});
-								})
-								.error(function() {
-									console.log('erreur lors du telechargement de la page catalogue');
-								});
-							// $('body').html().toString();
-						}).error(function() {
-							console.log('une erreur');
-						});
-					} else {
-						console.log('from page');
-						$scope.listDocument = JSON.parse(document.getElementById('JSON').text);
-					}
-
+					var tmp2 = dropbox.shareLink('test.html', localStorage.getItem('compte'), 'sandbox');
+					tmp2.then(function(data) {
+						window.location.href = data.url + '#/listDocument';
+					});
 				}
 			} else {
-				if (localStorage.getItem('compte')) {
-					console.log('access token trouve');
-					$http.get('https://api.dropbox.com/1/search/sandbox/?access_token=tLV5CIPVEoAAAAAAAAAAAcZ5zmTIIKjC1VVmeR3zdMokH9dTnk_jIrmAm6oLaVsN&query=.html').success(function(data) {
-						$scope.listDocument = data;
+				$('#listDocumentPage').show();
+				if (navigator.onLine) {
+					if (localStorage.getItem('compte')) {
+						var tmp5 = dropbox.search('.html', localStorage.getItem('compte'), 'sandbox');
+						tmp5.then(function(data) {
+							$scope.listDocument = listDocument;
+							$scope.initialLenght = $scope.listDocument.length;
+							for (var i = 0; i < $scope.listDocument.length; i++) {
+								var documentExist = false;
+								for (var y = 0; y < data.length; y++) {
+									if ($scope.listDocument[i].path === data[y].path) {
+										console.log('ce document exist');
+										documentExist = true;
+									}
+								}
+								if (!documentExist) {
+									console.log('ce document nexist pas');
+									$scope.listDocument.splice(i);
+									//console.log(listDocument);
+									// entirePage = entirePage.replace(':v' + dataFromDownload.charAt(29), ':v' + newVersion);
+								}
+							}
+							console.log($scope.initialLenght + '============' + $scope.listDocument.length);
+							if ($scope.initialLenght !== $scope.listDocument.length) {
 
-						var myscript = document.createElement('script');
-						myscript.id = 'JSON';
-						myscript.text = JSON.stringify($scope.listDocument);
-						document.body.appendChild(myscript);
-						console.log(JSON.parse(document.getElementById('JSON').text));
-						$('#listDocumentPage').show();
-						// $('body').html().toString();
-					}).error(function() {
-						console.log('une erreur');
-					});
+								var tmp7 = dropbox.download('/test.html', localStorage.getItem('compte'), 'sandbox');
+								tmp7.then(function(entirePage) {
+									console.log(entirePage);
+									var debut = entirePage.search('var listDocument') + 18;
+									var fin = entirePage.indexOf('"}];', debut) + 3; //entirePage.search('"}];') + 3;
+									console.log(entirePage.charAt(fin));
+									console.log('====================================' + debut + '========' + fin);
+									console.log(entirePage.substring(debut, fin));
+									entirePage = entirePage.replace(entirePage.substring(debut, fin), '[]');
+									console.log(entirePage);
+									console.log('====================================');
+									entirePage = entirePage.replace('listDocument= []', 'listDocument= ' + angular.toJson(listDocument));
+									// console.log(entirePage);
+									// console.log('====================================');
+									var tmp6 = dropbox.upload('test.html', entirePage, localStorage.getItem('compte'), 'sandbox');
+									tmp6.then(function() {
+										var tmp3 = dropbox.download('/listDocument.appcache', localStorage.getItem('compte'), 'sandbox');
+										tmp3.then(function(dataFromDownload) {
+											console.log(dataFromDownload);
+											var newVersion = parseInt(dataFromDownload.charAt(29)) + 1;
+											console.log('9dima ' + dataFromDownload.charAt(29) + 'jdoide' + newVersion);
+											dataFromDownload = dataFromDownload.replace(':v' + dataFromDownload.charAt(29), ':v' + newVersion);
+											var tmp4 = dropbox.upload('listDocument.appcache', dataFromDownload, localStorage.getItem('compte'), 'sandbox');
+											tmp4.then(function() {
+												console.log('new manifest uploaded');
+												window.location.href = window.location.href + '?refresh=true';
+											});
+										});
+									});
+								});
+							}
 
+							// console.log(entirePage.search('var listDocument') + '---' + entirePage.charAt(entirePage.search('var listDocument')));
+							// console.log(entirePage.search('bytes"}];') + '---' + entirePage.charAt(entirePage.search('bytes"}];')));
+							$('#listDocumentPage').show();
+							$scope.listDocument = listDocument;
+						});
+					} else {
+						$location.path('/');
+					}
 				} else {
-					$location.path('/');
-				};
-				// console.log(window.location.href);
-				// var lien = window.location.href;
-				// var verif = false;
-				// if ((lien.indexOf('http://dl.dropboxusercontent.com') > -1)) {
-				// 	window.location=lien.replace('listDocument', '');
-				// } else {
-				// 	if ($location.path() !== '/') {
-				// 		$location.path('/');
-				// 	}
-				// }
+					console.log('you are offline');
+					/* jshint ignore:start */
+					$scope.listDocument = listDocument;
+					/* jshint ignore:end */
 
+				}
 			}
 		});
 	};
+	$scope.open = function(path) {
+		$scope.deleteLink = path;
+	};
 
+	$scope.suprimeDocument = function() {
+		if (localStorage.getItem('compte')) {
+			var tmp = dropbox.delete($scope.deleteLink, localStorage.getItem('compte'), 'sandbox');
+			tmp.then(function(result) {
+				$('#myModal').modal('hide');
+				console.log(result);
+			});
+		}
+
+	};
 });
