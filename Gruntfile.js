cff0fc44df692cca5c827ba49cac1f4f39d3671a
mandoubi <mohamed.elmandoubi@neoxia.com>
2014-03-31 13:26:43 +0000

    Ajouter ".appcache" au goal "copy"

--- Gruntfile.js
+++ Gruntfile.js
@@ -200,7 +200,7 @@ module.exports = function(grunt) {
                     cwd: '<%= yeoman.app %>',
                     dest: '<%= yeoman.dist %>',
                     src: [
-                        'app/**/*.{html,css,png,jpeg,GIF,jpg,eot,svg,ttf,woff}',
+                        'app/**/*.{html,css,png,jpeg,GIF,jpg,eot,svg,ttf,woff,appcache}',
                         'app/bower_components/**/*.js',
                         'api/**/*',
                         'models/**/*',
