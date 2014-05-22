895513cb7254254493cddab2b707dfc4942990a8
mandoubi <mohamed.elmandoubi@neoxia.com>
2014-05-22 08:49:30 +0100

    Changement de la configuration Karma temporairement en FireFox

--- karma.conf.js
+++ karma.conf.js
@@ -73,7 +73,7 @@ module.exports = function(config) {
     // - Safari (only Mac)
     // - PhantomJS
     // - IE (only Windows)
-    browsers: ['PhantomJS'],
+    browsers: ['Firefox'],
 
 
     // Continuous Integration mode
