4458ce4237eb03ea5df3c12cee0412647450e81f
mandoubi <mohamed.elmandoubi@neoxia.com>
2014-05-22 08:58:07 +0100

    Ajout de la configuration "PhantomeJS" : Test unitaires

--- karma.conf.js
+++ karma.conf.js
@@ -73,7 +73,7 @@ module.exports = function(config) {
     // - Safari (only Mac)
     // - PhantomJS
     // - IE (only Windows)
-    browsers: ['Firefox'],
+    browsers: ['PhantomJS'],
 
 
     // Continuous Integration mode
