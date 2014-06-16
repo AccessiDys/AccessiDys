8e3b84faa5dc9db3e8e883d49d44c5c69ce857c7
amoufaddel <abdelhaq.moufaddel@neoxia.com>
2014-06-16 14:57:26 +0100

    Redevelopper les tests unitaires frontend des tags suite à l'integration des niveaux et l'upload des pictos.

--- test/spec/frontend/tag.js
+++ test/spec/frontend/tag.js
@@ -24,6 +24,7 @@
  */
 
 'use strict';
+/* global jasmine */
 
 describe('Controller:TagCtrl', function() {
 	var $scope, controller;
@@ -37,6 +38,12 @@ describe('Controller:TagCtrl', function() {
 	var tag = {
 		_id: '52c588a861485ed41c000003',
 		libelle: 'TP'
+	};
+
+	var tag1 = {
+		_id: '52c588a861485ed41c44343',
+		libelle: 'Exercice',
+		niveau: 1
 	};
 
 	var compteId = 'hsqhbhjds3543skdksdsddsd';
@@ -57,57 +64,93 @@ describe('Controller:TagCtrl', function() {
 		$httpBackend.whenPOST(configuration.URL_REQUEST + '/updateTag').respond(tag);
 	}));
 
-	/* TagCtrl:afficherTag */
-
-	it('TagCtrl:afficherTags should set afficherTags function', function() {
-		expect($scope.afficherTags).toBeDefined();
+	it('TagCtrl:showDefaultNiveau', function() {
+		expect($scope.showDefaultNiveau).toBeDefined();
+		$scope.showDefaultNiveau(tag);
+		expect(tag.niveau).toBe(1);
 	});
 
-	it('TagCtrl:afficherTags should call /readTags on $scope.afficherTags()', inject(function($httpBackend) {
-		$scope.afficherTags();
-		$httpBackend.flush();
-	}));
+	it('TagCtrl:getLibelleNiveau', function() {
+		expect($scope.getLibelleNiveau).toBeDefined();
+		var libNiv = $scope.getLibelleNiveau(2);
+		expect(libNiv).toBe('Niveau 2');
+	});
 
-	it('TagCtrl:afficherTags should listTags be tags', inject(function($httpBackend) {
+	it('TagCtrl:clearTag', function() {
+		expect($scope.clearTag).toBeDefined();
+		$scope.clearTag();
+		expect($scope.showNiveauTag).toBe(true);
+	});
+
+	it('TagCtrl:afficherTags', inject(function($httpBackend) {
+		expect($scope.afficherTags).toBeDefined();
 		$scope.afficherTags();
 		$httpBackend.flush();
 		expect($scope.listTags.length).toBe(2);
 	}));
 
-	// /* TagCtrl:ajouterTag */
-
-	it('TagCtrl:ajouterTag should set ajouterTag function', function() {
-		expect($scope.ajouterTag).toBeDefined();
+	it('TagCtrl:setFiles', function() {
+		var elm = {
+			files: [{
+				'webkitRelativePath': '',
+				'lastModifiedDate': '2014-06-12T10:12:18.000Z',
+				'name': 'p4.pdf',
+				'type': 'image/PDF',
+				'size': 1208
+			}]
+		};
+		$scope.setFiles(elm);
+		expect($scope.files.length).toEqual(0);
+		elm = {
+			files: [{
+				'webkitRelativePath': '',
+				'lastModifiedDate': '2014-06-12T10:12:18.000Z',
+				'name': 'p4.png',
+				'type': 'image/png',
+				'size': 1208
+			}]
+		};
+		$scope.setFiles(elm);
+		expect($scope.files.length).toEqual(1);
 	});
 
-	it('TagCtrl:ajouterTag should call /addTag on $scope.ajouterTag()', inject(function($httpBackend) {
-		$scope.requestToSend.tag = tag;
+	it('TagCtrl:ajouterTag', function() {
+		expect($scope.ajouterTag).toBeDefined();
+		expect($scope.preAjouterTag).toBeDefined();
+		$scope.preAjouterTag();
+		$scope.tag = tag;
+		$scope.tag.libelle = '';
 		$scope.ajouterTag();
-		$httpBackend.flush();
-	}));
-
-	it('TagCtrl:ajouterTag should tag be $scope.tagAdded', inject(function($httpBackend) {
-		$scope.requestToSend.tag = tag;
+		$scope.tag = tag;
+		$scope.tag.libelle = 'Solution';
 		$scope.ajouterTag();
-		$httpBackend.flush();
-		expect(tag).toEqual($scope.tagFlag);
-	}));
+		$scope.tag = tag;
+		$scope.tag.libelle = 'Solution';
+		$scope.showNiveauTag = false;
+		$scope.tag.position = 1;
+		$scope.tag.niveau = undefined;
+		$scope.ajouterTag();
+		$scope.tag.libelle = 'Solution';
+		$scope.showNiveauTag = true;
+		$scope.tag.position = 1;
+		$scope.ajouterTag();
+		$scope.xhrObj = jasmine.createSpyObj('xhrObj', ['addEventListener', 'open', 'send']);
+		spyOn(window, 'XMLHttpRequest').andReturn($scope.xhrObj);
+		$scope.files = [{
+			'webkitRelativePath': '',
+			'lastModifiedDate': '2014-06-12T10:12:18.000Z',
+			'name': 'p4.png',
+			'type': 'image/png',
+			'size': 1208
+		}];
+		$scope.ajouterTag();
+		expect($scope.xhrObj.addEventListener).toHaveBeenCalled();
+		expect($scope.xhrObj.addEventListener.calls.length).toBe(2);
+	});
 
-	// /* TagCtrl:supprimerTag */
-
-	it('TagCtrl:supprimerTag should set supprimerTag function', function() {
+	it('TagCtrl:supprimerTag', inject(function($httpBackend) {
 		expect($scope.preSupprimerTag).toBeDefined();
 		expect($scope.supprimerTag).toBeDefined();
-	});
-
-	it('TagCtrl:supprimerTag should call /deleteTag on $scope.supprimerTag()', inject(function($httpBackend) {
-		$scope.requestToSend.deleteTag = tag;
-		$scope.supprimerTag();
-		$scope.preSupprimerTag(tag);
-		$httpBackend.flush();
-	}));
-
-	it('TagCtrl:supprimerTag should tag be $scope.tagFlag', inject(function($httpBackend) {
 		$scope.requestToSend.deleteTag = tag;
 		$scope.supprimerTag();
 		$scope.preSupprimerTag(tag);
@@ -115,26 +158,45 @@ describe('Controller:TagCtrl', function() {
 		expect(tag).toEqual($scope.tagFlag);
 	}));
 
-	// /* TagCtrl:modifierTag */
-
-	it('TagCtrl:modifierTag should set modifierTag function', function() {
+	it('TagCtrl:modifierTag', function() {
 		expect($scope.preModifierTag).toBeDefined();
+		$scope.preModifierTag(tag1);
+		expect($scope.showNiveauTag).toEqual(false);
 		expect($scope.modifierTag).toBeDefined();
+		$scope.fiche = tag1;
+		$scope.fiche.libelle = '';
+		$scope.modifierTag();
+		$scope.fiche = tag1;
+		$scope.fiche.libelle = 'Solution';
+		$scope.modifierTag();
+		$scope.fiche = tag1;
+		$scope.fiche.libelle = 'Solution';
+		$scope.showNiveauTag = false;
+		$scope.fiche.position = 1;
+		$scope.fiche.niveau = undefined;
+		$scope.modifierTag();
+		$scope.fiche.libelle = 'Solution';
+		$scope.showNiveauTag = true;
+		$scope.fiche.position = 1;
+		$scope.modifierTag();
+		$scope.xhrObj = jasmine.createSpyObj('xhrObj', ['addEventListener', 'open', 'send']);
+		spyOn(window, 'XMLHttpRequest').andReturn($scope.xhrObj);
+		$scope.files = [{
+			'webkitRelativePath': '',
+			'lastModifiedDate': '2014-06-12T10:12:18.000Z',
+			'name': 'p4.png',
+			'type': 'image/png',
+			'size': 1208
+		}];
+		$scope.modifierTag();
+		expect($scope.xhrObj.addEventListener).toHaveBeenCalled();
+		expect($scope.xhrObj.addEventListener.calls.length).toBe(2);
 	});
 
-	it('TagCtrl:modifierTag should call /updateTag on $scope.modifierTag()', inject(function($httpBackend) {
-		$scope.requestToSend.tag = tag;
-		$scope.modifierTag();
-		$scope.preModifierTag(tag);
-		$httpBackend.flush();
-	}));
-
-	it('TagCtrl:modifierTag should tag be $scope.tagFlag', inject(function($httpBackend) {
-		$scope.requestToSend.tag = tag;
-		$scope.modifierTag();
-		$scope.preModifierTag(tag);
-		$httpBackend.flush();
-		expect(tag).toEqual($scope.tagFlag);
-	}));
+	it('TagCtrl:uploadComplete', function() {
+		expect($scope.uploadComplete).toBeDefined();
+		$scope.uploadComplete();
+		expect($scope.showNiveauTag).toBe(true);
+	});
 
 });
