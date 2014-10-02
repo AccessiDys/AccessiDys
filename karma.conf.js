6e7bb8cbdd0c717a76b5bb15fce0953437609bff
anasYoubi <anas.youbi@neoxia.com>
2014-10-02 16:20:22 +0100

    ajout de jquery-ui dans la configuration de karma

--- karma.conf.js
+++ karma.conf.js
@@ -22,6 +22,7 @@ module.exports = function(config) {
       'app/bower_components/angular-gettext/dist/angular-gettext.min.js',
       'app/bower_components/ui.bootstrap/ui-bootstrap-tpls-0.9.0.js',
       'app/bower_components/jquery/jquery.js',
+      'app/bower_components/jqueryUI/jquery-ui.js',
       'app/bower_components/jquery/jquery.line.js',
       'app/bower_components/sass-bootstrap/js/modal.js',
       'app/bower_components/underscore/underscore.js',
