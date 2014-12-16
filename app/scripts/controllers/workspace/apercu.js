b3f2399843afaf067e4a03b7058d4923ebd1be46
mandoubi <mohamed.elmandoubi@neoxia.com>
2014-12-16 12:09:59 +0000

    Clean des logs

--- app/scripts/controllers/workspace/apercu.js
+++ app/scripts/controllers/workspace/apercu.js
@@ -118,11 +118,8 @@ angular.module('cnedApp').controller('ApercuCtrl', function($scope, $rootScope, 
 
     for (var i = 0; i < $scope.tags.length; i++) {
       if ($scope.tags[i].libelle === 'Paragraphe') {
-
         for (var j = 0; j < $scope.profiltags.length; j++) {
-
           if ($scope.profiltags[j] && $scope.profiltags[j].tag === $scope.tags[i]._id) {
-            console.log('Got IT');
             var style = $scope.profiltags[j].texte;
             $scope.styleParagraphe = style.substring(style.indexOf('<p') + 2, style.indexOf('>'));
             break;
@@ -430,9 +427,6 @@ angular.module('cnedApp').controller('ApercuCtrl', function($scope, $rootScope, 
       var style = $scope.profiltags[profiltag].texte;
       var currentTag = getTagById($scope.profiltags[profiltag].tag);
 
-      console.log('currentTag ==> ');
-      console.log(currentTag);
-
       if (currentTag) {
         libelle = currentTag.libelle; //$scope.profiltags[profiltag].tagName;
       } else {
@@ -441,9 +435,7 @@ angular.module('cnedApp').controller('ApercuCtrl', function($scope, $rootScope, 
 
       /* Cas de la règle NORMAL */
       if (libelle.match('^Paragraphe')) {
-        console.log('Paragraphe found ==> ');
         $scope.styleParagraphe = style.substring(style.indexOf('<p') + 2, style.indexOf('>'));
-        console.log($scope.styleParagraphe);
       }
 
       if (block.tag === $scope.profiltags[profiltag].tag) {
