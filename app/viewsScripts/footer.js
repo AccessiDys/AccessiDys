0bbc9120b7eabcf7fcbb0deedd5ed2b34ba70944
anasYoubi <anas.youbi@neoxia.com>
2014-12-25 10:15:27 +0000

    annotation IE

--- app/viewsScripts/footer.js
+++ app/viewsScripts/footer.js
@@ -2,7 +2,8 @@ var footerHTML= '<div class="footer">'+
 	'<p>CnedAdapt est un outil proposé par le CNED - Mentions légales - ©2014 CNED</p>'+
   '<table>'+
   '<tr>'+
-  '<td contenteditable="true">anootation'+
+  '<td >'+
+  '<div contenteditable="true">anootation<div>'+
   '</td>'+
   '</tr>'+
   '</table>'+
