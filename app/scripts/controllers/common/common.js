cfcf0bfbba9545240639ed8ba8a17839a713c5ab
anasYoubi <anas.youbi@neoxia.com>
2015-05-13 10:26:46 +0100

    ajout d'un ecran d'inscription sous l'url /signup

--- app/scripts/controllers/common/common.js
+++ app/scripts/controllers/common/common.js
@@ -355,7 +355,7 @@ angular.module('cnedApp').controller('CommonCtrl', function($scope, $rootScope, 
         } else {
           lien = window.location.href;
           if ($scope.browzerState) {
-            if ($location.path() !== '/' && $location.path() !== '/passwordHelp' && $location.path() !== '/detailProfil' && $location.path() !== '/needUpdate' && $location.path() !== '/mentions' && verif !== true) {
+            if ($location.path() !== '/' && $location.path() !== '/passwordHelp' && $location.path() !== '/detailProfil' && $location.path() !== '/needUpdate' && $location.path() !== '/mentions'&& $location.path() !== '/signup' && verif !== true) {
               $location.path('/');
             }
             if ($location.path() === '/detailProfil' && lien.indexOf('#/detailProfil') > -1 && $rootScope.loged !== true) {
