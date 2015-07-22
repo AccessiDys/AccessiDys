ef04f0f2cfe9feee412c2f25683cff3b835539e1
anasYoubi <anas.youbi@neoxia.com>
2015-07-22 12:29:44 +0100

    clean git repo

--- app/scripts/services/config.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('services.config', [])
-	.constant('configuration', {
-		URL_REQUEST: 'https://localhost:3000',
-		DROPBOX_TYPE: 'sandbox',
-		CATALOGUE_NAME:'adaptation.html'
-	});
