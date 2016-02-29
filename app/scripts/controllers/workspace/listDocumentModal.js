df8a3979d97e6f2f33a143690022dd882995690d
YONI Philippe <philippe-bertrand.yoni@capgemini.com>
2016-02-29 16:41:47 +0100

    RTU CNED-413: Champs de recherche dans la pop-up de la liste des docs

--- app/scripts/controllers/workspace/listDocumentModal.js
+++ app/scripts/controllers/workspace/listDocumentModal.js
@@ -11,4 +11,13 @@ angular.module('cnedApp').controller('listDocumentModalCtrl', function($scope, $
     $scope.closeModal = function(){
         $modalInstance.dismiss('cancel');
     };
+    $scope.searchQuery = {};
+    
+    /* Filtre sur le nom de document à afficher */
+    $scope.specificFilterForModal = function() {
+        // parcours des Documents
+        for (var i = 0; i < $scope.listDocument.length; i++) {
+            $scope.listDocument[i].showed = $scope.listDocument[i].filename.toLowerCase().indexOf($scope.searchQuery.query.toLowerCase()) !== -1;
+        }
+    };
 });
---
+++ test/spec/frontend/listDocumentModal.js
@@ -0,0 +1,82 @@
+/*
+ * File: listDocumentModal.js
+ * 
+ * Copyright (c) 2013-2016
+ * Centre National d’Enseignement à Distance (Cned), Boulevard Nicephore Niepce, 86360 CHASSENEUIL-DU-POITOU, France
+ * (direction-innovation@cned.fr)
+ *
+ * GNU Affero General Public License (AGPL) version 3.0 or later version
+ *
+ * This file is part of a program which is free software: you can
+ * redistribute it and/or modify it under the terms of the
+ * GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License along with this program.
+ * If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+/* global spyOn:false */
+'use strict';
+
+describe('Controller: listDocumentModalCtrl', function() {
+
+    // load the controller's module
+    beforeEach(module('cnedApp'));
+
+    var $scope, controller, modalInstance, contenu = 'TEST', raison = '/', titre = 'INFO', location, forcerFermeture = null;
+    // Initialize the controller and a mock scope
+    beforeEach(inject(function($controller, $rootScope) {
+        modalInstance = {
+            close : function() {
+                return;
+            },
+            dismiss: function() {
+                return;
+            }
+        };
+        location = {
+            path : function() {
+                return '/';
+            }
+        };
+        $scope = $rootScope.$new();
+        controller = $controller('listDocumentModalCtrl', {
+            $scope : $scope,
+            $modalInstance : modalInstance,
+            content : contenu,
+            reason : raison,
+            title : titre,
+            forceClose : forcerFermeture,
+            $location : location
+        });
+    }));
+
+    it('listDocumentModalCtrl:closeModal()', inject(function($controller) {
+        spyOn(modalInstance, 'dismiss');
+        $scope.closeModal();
+        expect(modalInstance.dismiss).toHaveBeenCalled();
+    }));
+    
+    it('listDocumentModalCtrl:specificFilterForModal()', inject(function($controller, $rootScope) {
+        $scope.listDocument = [{
+            'filename': 'Erreur'
+        },{
+            'filename': 'Test'
+        }];
+        spyOn($scope, 'specificFilterForModal').andCallThrough();;
+        $scope.searchQuery = {};
+        $scope.searchQuery.query = 'Erreur'
+        $scope.specificFilterForModal();
+        expect($scope.specificFilterForModal).toHaveBeenCalled();
+        expect($scope.listDocument[0].showed).toEqual(true);
+    }));
+
+});
