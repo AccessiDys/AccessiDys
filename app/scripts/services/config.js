8f84492428447fe4acd69ad5dabef5176204ddb4
Corentin Langlais <corentinlanglais@gmail.com>
2017-02-24 16:07:07 +0100

    fix #565 issue - Set date pattern in config file

--- app/scripts/services/config.js
+++ app/scripts/services/config.js
@@ -6,5 +6,6 @@ angular.module('services.config', [])
 		CATALOGUE_NAME:'adaptation.html',
 		DROPBOX_PATH: '/Applications/AccessiDys',
 		HOMEPAGE_PATH: '#/listDocument',
-		DEFAULT_PATH: '#/'
+		DEFAULT_PATH: '#/',
+		DATE_PATTERN: 'dd/MM/yyyy'
 	});
