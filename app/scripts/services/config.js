cbf073ceb3bc1cacb7996e2b91994072ac3a64bb
Corentin Langlais <corentinlanglais@gmail.com>
2017-03-13 08:34:23 +0100

    Setting GA ID

--- app/scripts/services/config.js
+++ app/scripts/services/config.js
@@ -2,11 +2,11 @@
 
 angular.module('services.config', [])
 	.constant('configuration', {
-		URL_REQUEST: 'https://localhost:3000',
+		URL_REQUEST: 'https://recette.accessidys.com',
 		CATALOGUE_NAME:'adaptation.html',
 		DROPBOX_PATH: '/Applications/AccessiDys',
 		HOMEPAGE_PATH: '#/listDocument',
 		DEFAULT_PATH: '#/',
 		DATE_PATTERN: 'dd/MM/yyyy',
-		GOOGLE_ANALYTICS_ID: 'UA-93349828-1'
+		GOOGLE_ANALYTICS_ID: 'UA-69091614-1'
 	});
