cfe2fb17a868d29fb58b9016b30cef0aa1b403f6
Corentin Langlais <corentinlanglais@gmail.com>
2017-03-21 07:08:29 +0100

    Bug fix after refatoring

---
+++ app/bower_components/ckeditor/config.js
@@ -0,0 +1,49 @@
+/**
+ * @license Copyright (c) 2003-2015, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or http://ckeditor.com/license
+ */
+CKEDITOR.editorConfig = function (config) {
+
+    // name pour Tableau : Table à rajouter dans la toolbar insert
+    config.toolbar = [
+        { name: 'styles', items: [ 'Format' ] },
+        { name: 'clipboard', groups: [ 'clipboard', 'undo' ], items: [ 'Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo' ] },
+        { name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ], items: [ 'BulletedList' ] },
+        { name: 'insert', items: [ 'Image', 'SpecialChar',  'Link'] },
+        { name: 'insert', items: [ 'ImageCut', 'Ocerisation' ] },
+        { name: 'insert', items: [ 'PageBreak' ] }
+    ];
+
+    config.toolbar_OcrVersion = [
+        ['Scayt']
+    ];
+
+    config.skin = 'moono';
+
+    config.language = 'fr';
+
+    config.extraPlugins = 'pastebase64,sharedspace,blockquote,link,imagecut,ocerisation';
+
+    config.format_tags = 'p;h1;h2;h3;h4;h5;h6';
+
+    config.resize_enabled = false;
+
+    //remove contextmenu,tabletools,colordialog,liststyle to enable native context menu
+    config.removePlugins = 'elementspath,magicline,contextmenu,tabletools,colordialog,liststyle';
+
+    if (this.name === 'editorOcr') {
+        config.height = '200px';
+    }
+    else {
+        config.height = '350px';
+		config.startupFocus = true;
+        config.sharedSpaces = {
+            top: 'editorToolbar',
+            bottom: 'editorBottom'
+        };
+    }
+
+
+    //enable native spellcheck
+    config.disableNativeSpellChecker = false;
+};
