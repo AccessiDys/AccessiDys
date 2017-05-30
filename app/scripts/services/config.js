e9269375b0ba141ed9c53d72fbdfc3392de29d9d
WITTY-CLA <corentin.langlais@witty-services.fr>
2017-05-30 13:39:56 +0200

    patch update

--- app/scripts/services/config.js
+++ app/scripts/services/config.js
@@ -3,6 +3,6 @@
 angular.module('services.config', [])
 	.constant('configuration', {
 		DATE_PATTERN: 'dd/MM/yyyy',
-		GOOGLE_ANALYTICS_ID: 'UA-69091614-112',
+		GOOGLE_ANALYTICS_ID: 'UA-69091614-1',
 		ENV: 'dev'
 	});
